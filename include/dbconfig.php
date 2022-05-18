d77ad5d2e3b51b0a8655af6d6ed132a7f5846d2a
sompasong vongthavone <sompasong@laotop.net>
2022-05-18 23:05:47 +0700

    Add member_section

--- include/dbconfig.php
+++ include/dbconfig.php
@@ -1,21 +1,19 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"], 1);
 
-$con = new mysqli($server, $username, $password, $db);
-$con->set_charset("utf8");
-
-// //ສ້າງການເຊື່ອມຕໍ່ຖານຂໍ້ມູນ
-// $con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+// $con = new mysqli($server, $username, $password, $db);
 // $con->set_charset("utf8");
-// //ກວດສອບການເຊື່ອມຕໍ່ ຖ້າຫາກຜິດພາດໃຫ້ສະແດງຂໍ້ຜິດພາດ
-// if($con->connect_error){
-//     die('Database Connection failed: '. $con->connect_error);
-// }
 
-//mysql://baa1297f06529c:8b12aaab@us-cdbr-east-05.cleardb.net/heroku_174c0b283fa36b4?reconnect=true
+//ສ້າງການເຊື່ອມຕໍ່ຖານຂໍ້ມູນ
+$con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+$con->set_charset("utf8");
+//ກວດສອບການເຊື່ອມຕໍ່ ຖ້າຫາກຜິດພາດໃຫ້ສະແດງຂໍ້ຜິດພາດ
+if($con->connect_error){
+    die('Database Connection failed: '. $con->connect_error);
+}
--- member_section.php
+++ member_section.php
@@ -4,20 +4,55 @@ $current_page='home';
 require_once __DIR__.'/include/define.php';
 require_once __DIR__.'/include/function.php';
 require_once __DIR__.'/include/dbconfig.php';
-require_once __DIR__.'/header.php';
 if(!islogin()){header('Location:login.php');}
 ?>
-
-<div class="container mt-3">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="image/favicon.png" type="image/png">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/brands.min.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/dataTables.bootstrap5.min.css">
+    <script defer src="js/all.js"></script>
+    <title>ລະບົບຈັດການຂໍ້ມູນສະມາຊິກກຳມະບານ ສກອ</title>
+</head>
+<body style="height: 100%;">
+<div class="container mt-3"  style="height: 100%;">
     
     <div class="row">
-        <div class="col-md-2 pe-5">
+        <div class="col-3">
             <img src="image/trade_union_logo.png" alt="" class="img-fluid">
-        </div>
-        <div class="col-md-8">
             <?php
-                $sql = "SELECT CONCAT(member.firstname,' ',member.lastname) AS fullname ,
+                $sql = "SELECT * FROM college WHERE col_id = ". $_SESSION['college_id'] ;
+                $rs = mysqli_query($con, $sql);
+                $row = mysqli_fetch_assoc($rs);
+                echo '<p class="text-center mt-3 mb-5">
+                    <b>'.$row['col_name'].'</b><br>
+                    '.$row['col_village'].'<br>
+                    '.$row['col_district'].'<br>
+                    '.$row['col_province'].'<br>
+                    '.$row['tel'].'<br>
+                    '.$row['email'].'<br>
+                </p>';
+            ?>
+
+            <button onclick="window.print();" class="col-12 mb-3 btn btn-warning d-print-none"><i class="fa-solid fa-print"></i> &nbsp; ສັ່ງພິມ</button>
+            <a href="logout.php" class="btn btn-danger col-12 d-print-none">ອອກລະບົບ</a>
+
+        </div>
+        <div class="col-8 offset-1">
+            <?php
+                $sql = "SELECT member.mem_id, 
+                        CONCAT(member.firstname,' ',member.lastname) AS fullname ,
                         groups.group_name,
+                        (SELECT DATE_FORMAT(member_in.issue_date,'%d/%m/%y') FROM member_in WHERE member_in.mem_id = member.mem_id) AS in_date,
+                        (SELECT DATE_FORMAT(member_out.issue_date,'%d/%m/%y') FROM member_out WHERE member_out.mem_id = member.mem_id) AS out_date,
                         (SELECT college.col_name FROM college WHERE college.col_id = groups.col_id) AS col_name
                         FROM member 
                         JOIN groups ON member.group_id = groups.id
@@ -25,8 +60,7 @@ if(!islogin()){header('Location:login.php');}
 
                 $rs = mysqli_query($con, $sql);
                 $row = mysqli_fetch_assoc($rs);
-
-                print_r($row);
+                $mem_id = $row['mem_id'];
             ?>
             <br>
             <small class="text-secondary">ຊື່ ນາມສະກຸນ</small>
@@ -34,19 +68,55 @@ if(!islogin()){header('Location:login.php');}
 
                 <hr>
 
-            <small class="text-secondary">ສັງກັດຮາກຖານ</small>
-            <h2><?= $row['col_name'] ?> - <?= $row['group_name'] ?></h2>
+            <div class="row">
+                <div class="col-6 p-1">
+                    <div class="bg-info p-3" style="border-radius: 25px;">
+                    <small class="text-white">ສັງກັດຮາກຖານ</small>
+                    <h5><?= $row['col_name'] ?></h5>
+                    (<?= $row['group_name'] ?>)
+                    </div>
+                </div>
 
+                <div class="col-6 p-1">
+                    <div class="bg-warning p-3" style="border-radius: 25px;">
+                    <small class="text-white">ຂໍ້ມູນຍົກຍ້າຍ</small>
+                    <h5>ເຂົ້າ: <?= $row['in_date'] ?></h5>
+                    <h5>ອອກ: <?= $row['in_date'] ?></h5>
+                    </div>
+                </div>
+            </div>
+            
+            <h4  class="mt-5">ການຊຳລະຄ່າສະຕິ</h4>
             <?php
-                $sql = "SELECT membership_fee.pay_date, yearly_fee.fee FROM membership_fee JOIN "
+                $sql = "SELECT DATE_FORMAT(membership_fee.pay_date,'%d/%m/%Y') AS pd, yearly_fee.fee FROM membership_fee JOIN yearly_fee ON membership_fee.fee_id = yearly_fee.id WHERE mem_id = $mem_id";
+                $rs = mysqli_query($con, $sql);
+                
+                if(mysqli_num_rows($rs)==0){
+                    echo '<p>ຍັງບໍ່ທັນຊຳລະຄ່າສະຕິ</p>';
+                }else{
+                    echo '
+                        <table class="table table-striped table-bordered">
+                            <tr class="table-dark">
+                                <td>ວັນທີຊຳລະ</td>
+                                <td>ຍອດຊຳລະ</td>
+                            </tr>
+                    ';
+                    while($row = mysqli_fetch_assoc($rs)){
+                        echo '
+                            <tr>
+                                <td>'.$row['pd'].'</td>
+                                <td>'.number_format($row['fee']).'</td>
+                            </tr>
+                        ';
+                    }
+                    echo '
+                        </table>
+                    ';
+                }
             ?>
-
         </div>
     </div>
-
 </div>
-
-
 <?php
 require __DIR__.'/footer.php';
 ?>
