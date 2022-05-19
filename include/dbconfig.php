7bc1231ebbfe584c9d9d6128c4cd24ffd7951af0
sompasong vongthavone <sompasong@laotop.net>
2022-05-20 04:52:47 +0700

    change function and dbconfig

---
+++ include/dbconfig.php
@@ -0,0 +1,20 @@
+<?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+$con->set_charset("utf8");
+
+//ສ້າງການເຊື່ອມຕໍ່ຖານຂໍ້ມູນ
+// $con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+// $con->set_charset("utf8");
+//ກວດສອບການເຊື່ອມຕໍ່ ຖ້າຫາກຜິດພາດໃຫ້ສະແດງຂໍ້ຜິດພາດ
+// if($con->connect_error){
+//     die('Database Connection failed: '. $con->connect_error);
+// }
